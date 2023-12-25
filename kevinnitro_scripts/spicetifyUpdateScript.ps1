5411e1e34ee338f92dbd932a54237042e2409cc7
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-12-25 18:01:50 +0700

    Update Windows Dotfiles at 6:01 PM on 25/12/23

--- kevinnitro_scripts/spicetifyUpdateScript.ps1
+++ kevinnitro_scripts/spicetifyUpdateScript.ps1
@@ -19,13 +19,21 @@ spicetify.exe update
 
 # Update extensions
 
+echo "Update Volume Percentage"
 Update-Extension "volumePercentage.js" "https://raw.githubusercontent.com/daksh2k/Spicetify-stuff/master/Extensions/volumePercentage.js"
+echo "Done updating Volume Percentage!"
+
+echo "Update Beautiful Lyrics"
 Update-Extension "beautiful-lyrics.js" "https://github.com/surfbryce/beautiful-lyrics/raw/main/dist/beautiful-lyrics.js"
+echo "Done updating Beautiful Lyrics!"
 
 # Theme update
 
+echo "Update Themes - Catppuccin"
+Remove-Item -Path "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin" -Recurse
 curl -o "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin.zip" "https://codeload.github.com/catppuccin/spicetify/zip/refs/heads/main"
 Expand-Archive -Path "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin.zip" -DestinationPath "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin-extracted"
 Move-Item -Path "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin-extracted\\spicetify-main\\catppuccin" -Destination "$env:LOCALAPPDATA\\spicetify\\Themes\\"
 Remove-Item -Path "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin.zip"
 Remove-Item -Path "$env:LOCALAPPDATA\\spicetify\\Themes\\catppuccin-extracted" -Recurse
+echo "Done updating Themes - Catppuccin!"
